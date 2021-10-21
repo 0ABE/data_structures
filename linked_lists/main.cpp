455934397cd5b46957b3fb83ecdea519672ef067
Abe Mishler <intabeh_github@mishleremail.com>
2021-10-21 01:24:02 -0400

    initial commit

---
+++ linked_lists/main.cpp
@@ -0,0 +1,13 @@
+#include "Node.h"
+#include "reverse_list_1.cpp"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+  ReverseList_1 reverse_list_1;
+  reverse_list_1.run();
+
+  return 0;
+}
